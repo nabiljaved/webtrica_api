8620686be2e065275a9497cf8108e20f3611f05c
Muhammad Salman <64697477+SalmanShaikh2890@users.noreply.github.com>
2021-02-08 13:45:30 +0500

    for modification

--- models/Package.js
+++ models/Package.js
@@ -1,33 +1,45 @@
 const mongoose = require('mongoose')
 
 const packageSchema = new mongoose.Schema({
-
-    title : {
+    location : {
         type: String,
         required : true,
         trim : true, 
     },
-    description : {
+    package : {
         type: String,
         required : true,
         trim : true, 
     },
-    imageUrl : {
-        type: String,
-        required : true,
-        trim : true, 
-    },
-    promotionCode : {
-        type: String,
-        required : true,
-        trim : true, 
-    },
-    price : {
-        type: String,
-        required : true,
-        trim : true, 
-    },
-    
+    options : [
+        {
+            title : {
+                type: String,
+                required : true,
+                trim : true, 
+            },
+            description : {
+                type: String,
+                required : true,
+                trim : true, 
+            }, 
+            price : {
+                type: String,
+                required : true,
+                trim : true, 
+            },
+            valid : {
+                type: String,
+                required : true,
+                trim : true, 
+            },
+            expires : {
+                type: String,
+                required : true,
+                trim : true, 
+            },
+        }
+    ],
 }, {
     timestamps : true
 })
